c373bc0b5b9a9b19bb9fb1a55f71bc64777db6b7
ItsTheWhale <103934114+ItsTheWhale@users.noreply.github.com>
2023-12-03 17:21:40 +0800

    Create part1.js

---
+++ day3/part1.js
@@ -0,0 +1,68 @@
+import { input } from "./input.js";
+
+const lines = input.split("\n");
+const chars = lines.map(l => l.split(''));
+
+let sum = 0;
+
+const symbolRegex = /[^\.0-9]/;
+
+for (let y in lines) {
+    y = Number(y);
+
+    const numberRegex = /[0-9]+/g;
+
+    let numberMatch;
+
+    while (numberMatch = numberRegex.exec(lines[y])) {
+        console.log(numberMatch[0])
+        let isPart = false;
+        for (let x = numberMatch.index; x < numberMatch.index + numberMatch[0].length; x++) {
+            if (
+                (y > 0 && (chars[y - 1][x].match(symbolRegex)
+                    || (x > 0 && chars[y - 1][x - 1].match(symbolRegex))
+                    || (x < chars[y - 1].length - 2 && chars[y - 1][x + 1].match(symbolRegex)))
+                )
+                || (x > 0 && chars[y][x - 1].match(symbolRegex))
+                || (x < chars[y].length - 2 && chars[y][x + 1].match(symbolRegex))
+                || (y < chars.length - 2 && (chars[y + 1][x].match(symbolRegex)
+                    || (x > 0 && chars[y + 1][x - 1].match(symbolRegex))
+                    || (x < chars[y + 1].length - 2 && chars[y + 1][x + 1].match(symbolRegex)))
+                )
+            ) {
+                isPart = true;
+                break;
+            }
+        }
+
+        if (isPart) sum += Number(numberMatch);
+    }
+}
+
+console.log(sum);
+
+// const symbolRegex = /[^\.0-9]/;
+
+// for (let y in chars) {
+//     y = Number(y);
+//     for (let x in chars[y]) {
+//         x = Number(x);
+//         if (chars[y][x] === '.' || isNaN(Number(chars[y][x]))) continue;
+
+//         if (
+//             (y > 0 && (chars[y - 1][x].match(symbolRegex)
+//                 || (x > 0 && chars[y - 1][x - 1].match(symbolRegex))
+//                 || (x < chars[y - 1].length - 2 && chars[y - 1][x + 1].match(symbolRegex)))
+//             )
+//             || (x > 0 && chars[y][x - 1].match(symbolRegex))
+//             || (x < chars[y].length - 2 && chars[y][x + 1].match(symbolRegex))
+//             || (y < chars.length - 2 && (chars[y + 1][x].match(symbolRegex)
+//                 || (x > 0 && chars[y + 1][x - 1].match(symbolRegex))
+//                 || (x < chars[y + 1].length - 2 && chars[y + 1][x + 1].match(symbolRegex)))
+//             )
+//         ) {
+//             console.log(chars[y][x] + 'adjacent');
+
+//         }
+//     }
+// }
